db3c2aace915e36aaf500ab65952ab1e1a1a9212
Haoming-jpg <1669935064@qq.com>
2023-03-06 13:57:29 -0500

    Collision service create

---
+++ frontend/src/app/constants/constant.ts
@@ -0,0 +1,4 @@
+// build the collision index
+let collisions: string[] = [];
+
+export const COLLISIONS = collisions;
--- frontend/src/app/pages/chat-town/chat-town.component.ts
+++ frontend/src/app/pages/chat-town/chat-town.component.ts
@@ -6,6 +6,7 @@ import { UtilsService } from 'src/app/services/utils.service';
 import { Player } from '../../classes/player';
 import { KeyPressListener } from 'src/app/classes/key-press-listener';
 import * as PIXI from 'pixi.js';
+import { CollisionsService } from '../../services/collisions.service';
 
 @Component({
   selector: 'app-chat-town',
@@ -54,7 +55,8 @@ export class ChatTownComponent {
     private db: AngularFireDatabase,
     private afAuth: AngularFireAuth,
     private authService: AuthService,
-    private Utils: UtilsService
+    private Utils: UtilsService,
+    private Collisions: CollisionsService
   ) {}
 
   ngOnInit(): void {
@@ -181,9 +183,10 @@ export class ChatTownComponent {
   }
 
   handleArrowPress(direction: string) {
-    if (true) {
+    const mePlayer = this.allPlayers[this.playerId];
+    console.log(mePlayer.x, mePlayer.y);
+    if (!this.Collisions.checkCollisions(mePlayer, direction)) {
       //move to the next space
-      const mePlayer = this.allPlayers[this.playerId];
       mePlayer.update({
         direction: direction,
         cameraPerson: this.allPlayersRef[this.playerId],
