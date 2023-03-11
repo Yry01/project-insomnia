8dac5648387253ff0ee4a31c6dbfd200a22d9a28
Xu Zheng <barry.zheng@mail.utoronto.ca>
2023-03-11 01:50:27 -0500

    connect to redis DB

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,24 @@ import bodyParser from "body-parser";
 import { Server } from "socket.io";
 import http from "http";
 import dotenv from "dotenv";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 export const app = express();
 app.use(bodyParser.json());
+
+// connect to redis
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.connect();
 
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
